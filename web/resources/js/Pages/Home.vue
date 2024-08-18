643bb5a1085b3a9705f10342d21bff56075585ae
Diego Henrique de Oliveira Madero <60300499+diegogit03@users.noreply.github.com>
2024-08-18 01:12:56 -0300

    setup inertia and shadcn

---
+++ web/resources/js/Pages/Home.vue
@@ -0,0 +1,8 @@
+<template>
+    <h1>Hello World!</h1>
+    <Button>Teste</Button>
+</template>
+
+<script setup>
+    import { Button } from '@/components/ui/button';
+</script>
--- web/vite.config.js
+++ web/vite.config.js
@@ -1,11 +1,34 @@
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
+import vue from '@vitejs/plugin-vue';
+import path from 'path';
+
+import tailwind from 'tailwindcss'
+import autoprefixer from 'autoprefixer'
 
 export default defineConfig({
+    resolve: {
+        alias: {
+            "@": path.resolve(__dirname, './@')
+        }
+    },
+    css: {
+        postcss: {
+          plugins: [tailwind(), autoprefixer()],
+        },
+      },
     plugins: [
         laravel({
             input: ['resources/css/app.css', 'resources/js/app.js'],
             refresh: true,
+        }),
+        vue({
+            template: {
+                transformAssetUrls: {
+                    base: null,
+                    includeAbsolute: false,
+                },
+            },
         }),
     ],
 });
